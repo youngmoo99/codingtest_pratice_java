9fdc774148d43710229b7cefac3ad9e69fa8b2ab
youngmoo99 <rladuan612@gmail.com>
2023-12-16 17:36:04 +0900

    day2 commit
    2023-12-16 commit

---
+++ Class part1/src/ch02시간복잡도/개미.java
@@ -0,0 +1,53 @@
+package ch02시간복잡도;
+
+import java.util.Scanner;
+
+public class 개미 {
+
+	public static void main(String[] args) {
+		// 백준10158
+		//시간복잡도 생각 해야함 
+		// 1<= T <=200000 
+		//2 <= W,H <= 40000
+		// 계속하다보면 원점으로 다시 돌아옴 -> 다시 돌아오는 주기를 찾아야함 
+		
+		Scanner sc = new Scanner(System.in);
+		int w = sc.nextInt();
+		int h = sc.nextInt();
+		int p = sc.nextInt();
+		int q = sc.nextInt();
+		int t = sc.nextInt();
+		
+		int timex = t %(2 * w);
+		
+		int currentX = p;
+		
+		int deltaX = 1;
+	
+		
+		while(timex-- > 0) { //0이 될때까지 반복
+			if(currentX == w)
+				deltaX = -1;
+			else if(currentX == 0)
+				deltaX = 1;
+			currentX += deltaX;
+		}
+		
+		
+		int deltaY = 1;
+		int currentY = q;
+		int timey = t% (2 * h);
+		while(timey-- > 0) { //0이 될때까지 반복
+			if(currentY == h)
+				deltaY = -1;
+			else if(currentY == 0)
+				deltaY = 1;
+			currentY += deltaY;
+		}
+		
+		System.out.println(currentX + " "+ currentY);
+		
+		
+	}
+
+}
---
+++ Class part1/src/ch02시간복잡도/시간복잡도란.java
@@ -0,0 +1,17 @@
+package ch02시간복잡도;
+
+public class 시간복잡도란 {
+
+	public static void main(String[] args) {
+		// 시간복잡도
+		// 입력크기와 알고리즘간의 관계 
+		// 입력 크기에 대해 프로그램의 동작시간을 가늠해볼 수 있는 수단 
+		
+		//Big-O / Big-Omega / Big-Theta 와 같은 표기법으로 나타낼 수 있다. 
+		//-강의에서 다루는것은 Big-O
+		// 정의된 입력 데이터 중 가장 최악의 상황을 포함한 시간의 상한선 
+		
+		
+	}
+
+}
